c85cea65652ae0757b24a492260bf46b865636e2
tafiranium <misha.zaharov.2016@gmail.com>
2024-08-26 22:27:40 +0300

    Update interface.js

--- files_to_inject/interface.js
+++ files_to_inject/interface.js
@@ -125,7 +125,7 @@ class Interface {
               let problem = prompt("Опишите проблему");
               result = confirm("Отправить отчет?");
               if (result != false) {
-                sendMessage(`${problem}\n${window.location.href}\n${this.html}`)
+                sendMessage(`${problem}\n${window.location.href}\n${this.html.textContent}`)
                 alert("Спасибо за уделенное время! Приятного использования!");
               } 
             }
