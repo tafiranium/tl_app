f5f7c8f79dd4f00dadfbd4c7a7f58d1dcde92036
tafiranium <misha.zaharov.2016@gmail.com>
2024-09-05 14:42:25 +0300

    Update anal.js

--- files_to_inject/anal.js
+++ files_to_inject/anal.js
@@ -40,14 +40,26 @@ class AnalIs {
         Object.keys(this.temp).forEach(element => {this.ts["t" + element.replace("_", "")] = this.temp[element]});
         log("this.temp -> obj.keys() -> this.ts", [this.temp, this.ts], [this.CLASS_NAME, FU_NAME])
 
-        this.type_of_page = this.TypeOfPage()
+        this.type_of_page_object = new Type(this.html, this.cfg, this.temp, this.all_tables_sorted[0]["return"])
+        this.type_of_page = this.type_of_page_object.type
+        this.return_type = false
+
+        if (this.type_of_page[0] == "return") {
+
+            let page = await this.get_return_bill()
+            this.return_type = new Type(page, this.cfg, this.temp, true)
+
+            this.vars = this.return_type.vars
+        }
+
+        this.copy_massive = [this.return_type, this.vars]
+
         log("this.type_of_page: ", this.type_of_page, [this.CLASS_NAME, FU_NAME]), 
         log("this.temp: ", this.temp, [this.CLASS_NAME, FU_NAME])
-
         this.current_temp = this.temp[this.type_of_page[0]][0]
-        log("this.current_temp: ", this.current_temp, [this.CLASS_NAME, FU_NAME])
 
-        for (let i=1; i<this.current_temp.length; i++) {
+        log("this.current_temp: ", this.current_temp, [this.CLASS_NAME, FU_NAME])
+        for (let i = 1; i < this.current_temp.length; i++) {
             let e = this.current_temp[i]
             if (e.length == 1 & e[0] == false) {
                 this.current_temp[i] = Array(this.def[2][i]).fill(-1)
@@ -60,6 +72,15 @@ class AnalIs {
         this.Scan(this.current_temp) 
         return [this.type_of_page, this.vp]
     }
+
+    async get_return_bill(url) {
+        let bill = this.html.querySelector("#detail-view tr:nth-child(17) a")
+        let bill_url = bill.getAttribute("href")
+        let page = await get_page(bill_url)
+        let html = document.createElement("div")
+        html.innerHTML = page
+        return html
+    } 
 
     default_group(temp) {
         
@@ -78,8 +99,10 @@ class AnalIs {
 
         if (this.def[0]["date"]) {this.vp[this.def[1]["date"]] = this.tm[1]} // дата   
         if (this.def[0]["time"]) {this.vp[this.def[1]["time"]] = this.tm[2]} // время 
+
         let seller = this.all_tables_sorted[0]["seller"].trim().split(" ").filter((e) => !e.includes("-")).join(" ")
         let seller_hash = btoa(unescape(encodeURIComponent(seller)))
+
         if (Object.keys((this.cfg["enames"])).includes(seller_hash)) seller = decodeURIComponent(escape(atob(this.cfg["enames"][seller_hash])))
         if (this.def[0]["name"]) {this.vp[this.def[1]["name"]] = seller} // имя продавца
 
@@ -106,12 +129,13 @@ class AnalIs {
         this.standart(temp, otemp)
     }
 
-    items_group(temp) {
+    items_group(temp) { // 3
         
         let FU_NAME = "items_group"
         log("async function", `${FU_NAME}(${temp})`, [this.CLASS_NAME, FU_NAME])
 
         let items = this.all_tables_sorted[2]
+
         if (Object.keys(items).length > 0) {
 
             let desc = []
@@ -123,12 +147,16 @@ class AnalIs {
                     if (!(this.cfg["stop"].includes(e.art))) {
                         desc.push(e.name); arts.push(e.art); if (e.is_good) {count+=Number(e.count);}
                     }
-                } else {desc.push(e.name); arts.push(e.art); if (e.is_good) {count+=Number(e.count)};}
+                } else if (temp[3] == -1) {
+                    desc.push(e.name); arts.push(e.art); if (e.is_good) {count+=Number(e.count)};
+                }
             }); 
             
             if (temp[1] != 1) {desc = []}
+
             let comment_245 = this.all_tables_sorted[0]["comment"].replace(/[\r\n\t]+/g, " ")
             let c = document.createElement("div")
+
             c.innerHTML += comment_245
             comment_245 = c.innerText
 
@@ -143,15 +171,18 @@ class AnalIs {
 
             if (temp[2] == -1) {this.vp[29] = temp[2]}
             if (temp[0] != 1) { this.vp[27] = temp[0]; this.vp[28] = temp[0] } 
+
             else { 
 
                 if (count > 0) { this.vp[27] = 1; this.vp[28] = count; this.refuse_count = true;
                 } else { 
+
                     this.refuse_count = false; 
+
                     this.vp[27] = 0; this.vp[28] = 0 
                     this.vp[this.all_list[this.traffic]] = 0; this.vp[3] = 0;
+
                 }
-                
             }
         }
     }
@@ -197,8 +228,10 @@ class AnalIs {
         let cash_nocash = cnc[1]
         let cash_nocash_sbp = cnc[0]
 
+        this.money_object = ((temp.length == 1) & (temp[0] == -1)) ? { 37: ["-1", "-1"], 38: ["-1", "-1"] } : false
+
         // обычная оплата 
-        if (temp[0] === 1) {
+        if (temp[0] === 1 & !this.money_object) {
             this.vp[30] = cash_nocash_sbp[0][0]
             this.vp[31] = cash_nocash_sbp[1][0]
             this.vp[32] = cash_nocash_sbp[2][0]
@@ -218,14 +251,32 @@ class AnalIs {
             }
 
         // возврат заказа
-        } else if (temp[2] === 1) {
-            this.vp[37] = cash_nocash[0][0]
-            this.vp[38] = cash_nocash[1][0]
-            this.money_object = {
-                37: cnc[1][0],
-                38: cnc[1][1]
+        } else if (this.return_type != false & (temp[2] === 1)) {
+            if (this.return_type.type[0] != false) {
+                if (this.return_type.type[0] == "mobile") {
+                    this.vp[33] = cash_nocash[0][0]
+                    this.vp[34] = cash_nocash[1][0]
+                    this.money_object = {
+                        33: cnc[1][0],
+                        34: cnc[1][1]
+                    }
+                } else if (this.return_type.type[0] == "buyer") {
+                    this.vp[37] = cash_nocash[0][0]
+                    this.vp[38] = cash_nocash[1][0]
+                    this.money_object = {
+                        37: cnc[1][0],
+                        38: cnc[1][1]
+                    }
+                } else {
+                    this.vp[37] = cash_nocash[0][0]
+                    this.vp[38] = cash_nocash[1][0]
+                    this.money_object = {
+                        37: cnc[1][0],
+                        38: cnc[1][1]
+                    }
+                }      
             }
-        }
+        } 
     }
 
     Scan(temp) {
@@ -234,65 +285,10 @@ class AnalIs {
         log("async function", `${FU_NAME}(${temp})`, [this.CLASS_NAME, FU_NAME])
 
         this.default_group(temp[0])
-        this.enter_group(temp[1], this.current_temp[2])
+        this.enter_group(temp[1], temp[2])
         this.items_group(temp[3])
         this.money_group(temp[4])
         return this.vp
+
     } 
-
-    TypeOfPage() {
-        
-        let FU_NAME = "TypeOfPage"
-        log("async function", `${FU_NAME}()`, [this.CLASS_NAME, FU_NAME])
-
-        function one_check(name, ats) {
-            let siz = Object.keys(ats[2]).length
-            if (siz <= 1) {if (ats[2][0].name === name) {return true}}
-            return false
-        }
-
-        function check(arg) {
-            if (arg["type"] == 1) {return (Object.keys(arg["template"][arg["part"]][2]).includes(arg["all_tables"][0]["traffic"]))}
-            if (arg["type"] == 2) {return one_check(arg["argument"], arg["all_tables"])}
-            if (arg["type"] == 3) {return (arg["all_tables"][arg["argument"][0]][arg["argument"][1]]
-                    .classList.contains(arg["argument"][2]) != true)}
-        } 
-
-        let cluv = {} 
-        log("this.temp: ", this.temp, [this.CLASS_NAME, FU_NAME])
-        Object.keys(this.temp).forEach(element => {
-
-            let cfg = this.temp[element][1]
-            
-            let send = {
-                "type": cfg[0], 
-                "argument": cfg[1], 
-                "part": element, 
-                "all_tables": this.all_tables_sorted, 
-                "template": this.temp
-            }   
-
-            cluv[element] = check(send)
-
-        }); cluv["empty"] = false
-        
-        let t = this.all_tables_sorted[0]
-        let enter = this.cfg["enter"].includes(t["traffic"])
-        let dc = !!((t["dc"] != "Не задан") & (t["buyer"] != "Не задан"))
-    
-        let send = [enter, dc, this.cfg["points"][this.all_tables_sorted[0]["shop"]]]
-
-        let end_r = (el) => {if (cluv[el]) {return [el, send]} else return false}
-        let all_temps =     Object.keys(this.temp)
-        let no_buyer  =     this.cfg["no_buyer"]
-
-        let result = false
-
-        all_temps.filter(x => !no_buyer.includes(x)).forEach(x => {let s = end_r(x); if (s) {result = s}});
-        no_buyer.forEach(x => {let s = end_r(x); if (s) {result = s}});
-
-        if (!result) {result = "empty"}
-        log("result: ", result, [this.CLASS_NAME, FU_NAME])
-        return result
-    }
 }
